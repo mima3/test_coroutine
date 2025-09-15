b48ea0039850a67d1c5e1858d47a39c75659832a
板垣正樹 <7877000+mima3@users.noreply.github.com>
2025-09-15 11:43:24 +0900

    pythonの修正

--- simple_sample/python313/generator03.py
+++ simple_sample/python313/generator03.py
@@ -3,17 +3,21 @@ def generator_inner1():
     yield 1
     yield 2
     yield 3
+    return 4
 
 def generator_inner2():
     print('  start generator_inner2 function')
     yield 11
     yield 12
     yield 13
+    return 14
 
 def generator_outer():
     print('start generator_outer function')
-    yield from generator_inner1()
-    yield from generator_inner2()
+    sub1 = yield from generator_inner1()
+    print('  sub generator1 return', sub1)
+    sub2 = yield from generator_inner2()
+    print('  sub generator2 return', sub2)
 
 for v in generator_outer():
     print("....", v)
